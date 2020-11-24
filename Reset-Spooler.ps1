ab64fba74abd11db5b59d87aeb5691c2c9aa6fa2
carloscondit <bazki.kz@mail.ru>
2020-11-24 12:20:21 +0300

    Базовая конфигурация командлета

---
+++ Reset-Spooler.ps1
@@ -0,0 +1,30 @@
+﻿function Reset-Spooler {
+    [CmdletBinding()]
+    param (
+        # Имя компьютера или список компьютеров
+        [Parameter(ValueFromPipeLine = $true)]
+        [String[]]$ComputerName="$env:computername"
+    )
+    
+    begin {
+        Write-Verbose "Начало работы командлета"
+    }
+    
+    process {
+        $Session = New-PSSession -ComputerName $ComputerName
+        $ScriptBlock = {
+            $ServiceName = 'Spooler'
+            Stop-Service -Name $ServiceName  
+            Remove-Item "C:\Windows\System32\spool\PRINTERS\*" -Recurse -Force
+            Start-Service -Name $ServiceName 
+        }
+
+        Invoke-Command -ComputerName $ComputerName -ScriptBlock $ScriptBlock
+        Remove-PSSession -Session $Session
+
+    }
+    
+    end {
+        Write-Verbose "Конец работы командлета" 
+    }
+}
